15b4c143dfc51bd78f3a865142e89683253e2c6c
jennlyle <jennifer.g.lyle@gmail.com>
2020-06-09 08:09:29 +0100

    Added post method from lecture notes

--- tasks.js
+++ tasks.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const serverless = require("serverless-http");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 app.use(cors());
+app.use(bodyParser.json());
 
 app.get("/tasks", function(req, res) {
   res.send({ tasks: ["water plants", "do dishes", "buy oats"] });
+});
+
+app.post("/tasks", function(req, res) {
+  const text = req.body.text;
+  const date = req.body.date;
+
+  res.json({
+    message: `Received a request to add task ${text} with date ${date}`
+  });
 });
 
 module.exports.handler = serverless(app);
