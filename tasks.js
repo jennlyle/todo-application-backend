e388e4f981f33fbd9f59ee16d7533b29bc55c28c
jennlyle <jennifer.g.lyle@gmail.com>
2020-06-13 14:08:17 +0100

    Completed Homework

--- tasks.js
+++ tasks.js
@@ -2,68 +2,91 @@ const express = require("express");
 const serverless = require("serverless-http");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const mysql = require("mysql");
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get("/tasks", function(req, res) {
-  // const text = req.body.text;
-  // const completed = req.body.completed;
-  // const date = req.body.date;
-  // const id = req.body.id;
 
-  const myGetTasksDummyData = { "tasks": {
-    "task": [
-      {
-        "text": "sweep floor",
-        "completed": true,
-        "date": "2019-10-10",
-        "id": 1
-      }, 
-      {
-        "text": "wash car",
-        "completed": false,
-        "date": "2019-10-09",
-        "id": 2
-      }, 
-      {
-        "text": "buy cat food",
-        "completed": true,
-        "date": "2019-10-08",
-        "id": 3
-      }, 
-      {
-        "text": "wash dishes",
-        "completed": true,
-        "date": "2019-10-08",
-        "id": 4
-      }, 
-    ]}
-  };
-  res.status(200).send(myGetTasksDummyData);
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: "todo_app"
+})
+
+app.get("/tasks", function(req, res) {
+  const query = "SELECT * FROM tasks";
+  connection.query(query, function(error, data) {
+    if(error) {
+      console.log("Error fetching tasks", error);
+      res.status(500).json({
+        error: error
+      });
+    } else {
+      res.status(200).json({
+        tasks: data
+      });
+    }
+  });
 });
 
 app.post("/tasks", function(req, res) {
-  const text = req.body.text;
-  const date = req.body.date;
-  res.status(201).send(`Received a request to add task ${text} with date ${date}`);
+  const query = "INSERT INTO `tasks` VALUES (?, ?, ?, ?)";
+  connection.query(query, [req.body.task_id, req.body.user_id, req.body.text, 
+      req.body.complete_status_id], function(error,data) {
+    if (error){
+        console.log("Error inserting task", error);
+        res.status(500).json({
+          error: error
+        });
+    }
+    else {
+        res.status(201).json({
+          data: data
+        });
+    }
+  });
 });
 
+
 app.delete("/tasks/:taskId", function(req, res) {
-  const id = req.params.taskId;
-  res.status(200).send(`You issued a delete request for ID: ${id}`);
+/*
+  For purposes of this week's homework, delete queries and such are written below.
+  However for purposes of my database, tasks are meant to be 'archived' so the 
+  query would be changed to an update query where complete_status_id is changed to 3.
+
+*/
+  const query = "DELETE FROM `tasks` WHERE (`tasks`.`task_id` = ?)";
+  connection.query(query, [req.params.taskId], function(error){
+    if (error){
+      console.log("Error deleting task", error);
+      res.status(500).json({
+        error: error
+      });
+    }
+    else {
+      res.sendStatus(200);
+    }
+  });
 });
 
 app.put("/tasks/:taskId", function(req, res) {
-  const id = req.params.taskId;
-  const text = req.body.text;
-  const completed = req.body.completed;
-  const date = req.body.date;
-
-  res.status(200).send(`You issued a put request for ID: ${id}`);
-
+  const query = "UPDATE `tasks` SET `tasks`.`user_id` = ?, `tasks`.`text` = ?, `tasks`.complete_status_id = ? WHERE (`tasks`.`task_id` = ?)";
+  
+  connection.query(query,[req.body.user_id, req.body.text, req.body.complete_status_id, req.params.taskId], function (error){
+    if (error){
+      console.log("Error updating task", error);
+      res.status(500).json({
+        error: error
+      });
+    }
+    else {
+      res.sendStatus(200);
+    }
+  });
 });
 
 module.exports.handler = serverless(app);
